9c2e97b7566671a5dc30fc321461f464376d7bc3
mmzk1526 <yc4120@ic.ac.uk>
2022-07-11 00:59:48 +0800

    [yc4120] (feat) Word8 parsers for both ASCII and Latin-1

--- src/MMZKBSParser/Word8.hs
+++ src/MMZKBSParser/Word8.hs
@@ -1,73 +1,41 @@
--- Utility parsers for single "Word8" tokens, which represents the Latin-1
--- character set.
+-- Utility parsers for single "Word8" ASCII and Latin-1 tokens.
 
 module MMZKBSParser.Word8 where
 
 import           Data.Word
 import           MMZKBSParser.Parser
 
+
+--------------------------------------------------------------------------------
+-- ASCII & Latin-1
+--------------------------------------------------------------------------------
+
 -- | Parse the given token ("Word8"). The result is retained as a "Word8".
 -- Use it with care if the list of tokens contains non-Latin-1 codepoints since
 -- it may extract a fragment of a codepoint.
 char :: Monad m => Word8 -> ParserT m Word8
-char = satisfy . (==)
-
--- | Parse any token ("Word8"). The result is retained as a "Word8".
-anyToken :: Monad m => ParserT m Word8
-anyToken = token Just
-{-# INLINE anyToken #-}
-
--- | Parse one token ("Word8") using the predicate. The result is retained as
--- a "Word8".
-satisfy :: Monad m => (Word8 -> Bool) -> ParserT m Word8
-satisfy f = token $ \x -> if f x then Just x else Nothing
-{-# INLINE satisfy #-}
+char = satisfyL1 . (==)
+{-# INLINE char #-}
 
 -- | Parse one of the given tokens ("Word8"). The result is retained as a
 -- "Word8".
 -- Use it with care if the list of tokens contains non-Latin-1 codepoints since
 -- it may extract a fragment of a codepoint.
 oneOf :: Monad m => Foldable t => t Word8 -> ParserT m Word8
-oneOf chs = satisfy (`elem` chs)
+oneOf chs = satisfyL1 (`elem` chs)
 {-# INLINE oneOf #-}
-
--- | Parse any token ("Word8") except those in the list. The result is retained
--- as a "Word8".
--- Use it with extreme care since it may extract a fragment of a codepoint if
--- the input "ByteString" contains non-Latin-1 characters.
-noneOf :: Monad m => Foldable t => t Word8 -> ParserT m Word8
-noneOf chs = satisfy (`notElem` chs)
-{-# INLINE noneOf #-}
-
--- | Parse one token ("Word8") representing an ASCII character, namely anything
--- below 128. The result is retained as a "Word8".
-ascii :: Monad m => ParserT m Word8
-ascii = satisfy (< 128)
-{-# INLINE ascii #-}
-
--- | Parse a token ("Word8") representing a control character, which is either
--- below 32 or in between 127 and 159. The result is retained as a "Word8".
-ctrl :: Monad m => ParserT m Word8
-ctrl = satisfy (\ch -> ch < 32 || (ch >= 127 && ch <= 159))
-{-# INLINE ctrl #-}
 
 -- | Parse a token ("Word8") representing the SPACE character (空格), namely 32.
 -- The result is retained as a "Word8".
 space32 :: Monad m => ParserT m Word8
-space32 = satisfy (== 32)
+space32 = satisfyL1 (== 32)
 {-# INLINE space32 #-}
 
 -- | Parse a token ("Word8") representing a space character (空白字符), namely
 -- \t, \n, \r, \f, \v, or  32. The result is retained as a "Word8".
 space :: Monad m => ParserT m Word8
-space = satisfy (\ch -> ch == 32 || (ch >= 9 && ch <= 13))
+space = satisfyL1 (\ch -> ch == 32 || (ch >= 9 && ch <= 13))
 {-# INLINE space #-}
-
--- | Parse a token ("Word8") representing a printable character, namely anything
--- except the control characters. The result is retained as a "Word8".
-printable :: Monad m => ParserT m Word8
-printable = satisfy (\ch -> (ch >= 32 && ch < 127) || ch > 159)
-{-# INLINE printable #-}
 
 -- | Parse a single digit: 0, 1, 2, 3, 4, 5, 6, 7, 8, or 9. The result is
 -- retained as a "Word8".
@@ -75,24 +43,104 @@ digit :: Monad m => ParserT m Word8
 digit = satisfy (\ch -> ch >= 48 && ch <= 57)
 {-# INLINE digit #-}
 
+-- | Parse a single hexadecimal digit: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9; a, b, c, d,
+-- e, f; A, B, C, D, E, F. The result is retained as a "Word8".
+hexDigit :: Monad m => ParserT m Word8
+hexDigit = satisfyL1 ( \ch -> (ch >= 48 && ch <= 57)
+                           || (ch >= 65 && ch <= 70)
+                           || (ch >= 97 && ch <= 102) )
+{-# INLINE hexDigit #-}
+
 -- | Parse a single octacal digit: 0, 1, 2, 3, 4, 5, 6, or 7. The result is
 -- retained as a "Word8".
 octDigit :: Monad m => ParserT m Word8
-octDigit = satisfy (\ch -> ch >= 48 && ch <= 55)
+octDigit = satisfyL1 (\ch -> ch >= 48 && ch <= 55)
 {-# INLINE octDigit #-}
 
 -- | Parse a single binary digit: 0 or 1. The result is retained as a "Word8".
 binDigit :: Monad m => ParserT m Word8
-binDigit = satisfy (\ch -> ch == 48 || ch == 49)
+binDigit = satisfyL1 (\ch -> ch == 48 || ch == 49)
 {-# INLINE binDigit #-}
 
--- | Parse a single hexadecimal digit: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9; a, b, c, d,
--- e, f; A, B, C, D, E, F. The result is retained as a "Word8".
-hexDigit :: Monad m => ParserT m Word8
-hexDigit = satisfy ( \ch -> (ch >= 48 && ch <= 57)
-                         || (ch >= 65 && ch <= 70)
-                         || (ch >= 97 && ch <= 102) )
-{-# INLINE hexDigit #-}
+
+--------------------------------------------------------------------------------
+-- ASCII Specific
+--------------------------------------------------------------------------------
+
+-- | Parse one token ("Word8") representing an ASCII character using the
+-- predicate. The result is retained as a "Word8".
+satisfy :: Monad m => (Word8 -> Bool) -> ParserT m Word8
+satisfy f = token $ \x -> if f x && x < 128 then Just x else Nothing
+{-# INLINE satisfy #-}
+
+-- | Parse any token ("Word8") representing an ASCII character except those in
+-- the list. The result is retained as a "Word8".
+noneOf :: Monad m => Foldable t => t Word8 -> ParserT m Word8
+noneOf chs = satisfy (`notElem` chs)
+{-# INLINE noneOf #-}
+
+-- | Parse one token ("Word8") representing an ASCII character, namely anything
+-- below 128. The result is retained as a "Word8".
+ascii :: Monad m => ParserT m Word8
+ascii = satisfy $ const True
+{-# INLINE ascii #-}
+
+-- | Parse a single letter: a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q,
+-- r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q,
+-- R, S, T, U, V, W, X, Y, Z. The result is retained as a "Word8".
+alpha :: Monad m => ParserT m Word8
+alpha = satisfy (\ch -> (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+{-# INLINE alpha #-}
+
+-- | Parse a single Latin letter or digit: a, b, c, d, e, f, g, h, i, j, k, l,
+-- m, n, o, p, q, r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L,
+-- M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9. The
+-- result is retained as a "Word8".
+alphaNum :: Monad m => ParserT m Word8
+alphaNum = satisfy ( \ch -> (ch >= 48 && ch <= 57)
+                         || (ch >= 65 && ch <= 90)
+                         || (ch >= 97 && ch <= 122) )
+{-# INLINE alphaNum #-}
+
+-- | Parse a single lowercase Latin letter: a, b, c, d, e, f, g, h, i, j, k, l,
+-- m, n, o, p, q, r, s, t, u, v, w, x, y, or z; The result is retained as a
+-- "Word8".
+lower :: Monad m => ParserT m Word8
+lower = satisfy (\ch -> ch >= 97 && ch <= 122)
+{-# INLINE lower #-}
+
+-- | Parse a single uppercase Latin letter: A, B, C, D, E, F, G, H, I, J, K, L,
+-- M, N, O, P, Q, R, S, T, U, V, W, X, Y, or Z. The result is retained as a
+-- "Word8".
+upper :: Monad m => ParserT m Word8
+upper = satisfy (\ch -> ch >= 65 && ch <= 90)
+{-# INLINE upper #-}
+
+
+--------------------------------------------------------------------------------
+-- Latin-1 Specific
+--------------------------------------------------------------------------------
+
+-- | Parse one token ("Word8") representing an ASCII character using the
+-- predicate. The result is retained as a "Word8".
+-- Use it with care if the list of tokens contains non-Latin-1 codepoints since
+-- it may extract a fragment of a codepoint.
+satisfyL1 :: Monad m => (Word8 -> Bool) -> ParserT m Word8
+satisfyL1 f = token $ \x -> if f x then Just x else Nothing
+{-# INLINE satisfyL1 #-}
+
+-- | Parse any token ("Word8") except those in the list. The result is retained
+-- as a "Word8".
+-- Use it with care if the list of tokens contains non-Latin-1 codepoints since
+-- it may extract a fragment of a codepoint.
+noneOfL1 :: Monad m => Foldable t => t Word8 -> ParserT m Word8
+noneOfL1 chs = satisfy (`notElem` chs)
+{-# INLINE noneOfL1 #-}
+
+-- | Parse any token ("Word8"). The result is retained as a "Word8".
+anyToken :: Monad m => ParserT m Word8
+anyToken = token Just
+{-# INLINE anyToken #-}
 
 -- | Parse a single letter: a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q,
 -- r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q,
@@ -100,12 +148,12 @@ hexDigit = satisfy ( \ch -> (ch >= 48 && ch <= 57)
 -- Ñ, Ò, Ó, Ô, Õ, Ö, Ø, Ù, Ú, Û, Ü, Ý; à, á, â, ã, ä, å, æ, ç, è, é, ê, ë, ì, í,
 -- î, ï, ð, ñ, ò, ó, ô, õ, ö, ø, ù, ú, û, ü, ý; ª, µ, º, Þ, ß, þ, ÿ. The result
 -- is retained as a "Word8".
-alpha :: Monad m => ParserT m Word8
-alpha = satisfy ( \ch -> (ch >= 65 && ch <= 90)
-             || (ch >= 97 && ch <= 122)
-             || ch == 170 || ch == 181 || ch == 186
-             || (ch >= 192 && ch /= 215 && ch /= 247) )
-{-# INLINE alpha #-}
+alphaL1 :: Monad m => ParserT m Word8
+alphaL1 = satisfyL1 ( \ch -> (ch >= 65 && ch <= 90)
+                 || (ch >= 97 && ch <= 122)
+                 || ch == 170 || ch == 181 || ch == 186
+                 || (ch >= 192 && ch /= 215 && ch /= 247) )
+{-# INLINE alphaL1 #-}
 
 -- | Parse a single Latin letter or digit: a, b, c, d, e, f, g, h, i, j, k, l,
 -- m, n, o, p, q, r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L,
@@ -113,29 +161,29 @@ alpha = satisfy ( \ch -> (ch >= 65 && ch <= 90)
 -- Ì, Í, Î, Ï, Ð, Ñ, Ò, Ó, Ô, Õ, Ö, Ø, Ù, Ú, Û, Ü, Ý; à, á, â, ã, ä, å, æ, ç, è,
 -- é, ê, ë, ì, í, î, ï, ð, ñ, ò, ó, ô, õ, ö, ø, ù, ú, û, ü, ý; ª, µ, º, Þ, ß, þ,
 -- ÿ; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9. The result is retained as a "Word8".
-alphaNum :: Monad m => ParserT m Word8
-alphaNum = satisfy ( \ch -> (ch >= 48 && ch <= 57)
-                         || (ch >= 65 && ch <= 90)
-                         || (ch >= 97 && ch <= 122)
-                         || ch == 170 || ch == 181 || ch == 186
-                         || (ch >= 192 && ch /= 215 && ch /= 247) )
-{-# INLINE alphaNum #-}
+alphaNumL1 :: Monad m => ParserT m Word8
+alphaNumL1 = satisfyL1 ( \ch -> (ch >= 48 && ch <= 57)
+                             || (ch >= 65 && ch <= 90)
+                             || (ch >= 97 && ch <= 122)
+                             || ch == 170 || ch == 181 || ch == 186
+                             || (ch >= 192 && ch /= 215 && ch /= 247) )
+{-# INLINE alphaNumL1 #-}
 
 -- | Parse a single lowercase Latin letter: a, b, c, d, e, f, g, h, i, j, k, l,
 -- m, n, o, p, q, r, s, t, u, v, w, x, y, z; ; à, á, â, ã, ä, å, æ, ç, è, é, ê,
 -- ë, ì, í, î, ï, ð, ñ, ò, ó, ô, õ, ö, ø, ù, ú, û, ü, ý; µ, ß, ÿ. The result is 
 -- retained as a "Word8".
-lower :: Monad m => ParserT m Word8
-lower = satisfy ( \ch -> (ch >= 97 && ch <= 122)
-                      || ch == 181
-                      || (ch >= 223 && ch /= 247) )
-{-# INLINE lower #-}
+lowerL1 :: Monad m => ParserT m Word8
+lowerL1 = satisfyL1 ( \ch -> (ch >= 97 && ch <= 122)
+                          || ch == 181
+                          || (ch >= 223 && ch /= 247) )
+{-# INLINE lowerL1 #-}
 
 -- | Parse a single uppercase Latin letter: A, B, C, D, E, F, G, H, I, J, K, L,
 -- M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z; À, Á, Â, Ã, Ä, Å, Æ, Ç, È, É, Ê, Ë,
 -- Ì, Í, Î, Ï, Ð, Ñ, Ò, Ó, Ô, Õ, Ö, Ø, Ù, Ú, Û, Ü, Ý; Þ. The result is retained
 -- as a "Word8".
-upper :: Monad m => ParserT m Word8
-upper = satisfy ( \ch -> (ch >= 65 && ch <= 90) 
-                      || (ch >= 192 && ch <= 222 && ch /= 215) )
-{-# INLINE upper #-}
+upperL1 :: Monad m => ParserT m Word8
+upperL1 = satisfyL1 ( \ch -> (ch >= 65 && ch <= 90) 
+                          || (ch >= 192 && ch <= 222 && ch /= 215) )
+{-# INLINE upperL1 #-}
