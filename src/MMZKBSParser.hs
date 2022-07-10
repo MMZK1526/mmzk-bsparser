30ee60fe205e4f3e668bc0ca1d9b774a48029e2f
mmzk1526 <yc4120@ic.ac.uk>
2022-07-10 22:12:40 +0800

    [yc4120] (feat) Refine some of the new parsers

--- src/MMZKBSParser.hs
+++ src/MMZKBSParser.hs
@@ -1,7 +1,8 @@
 module MMZKBSParser 
   ( ParserT, Parser, ByteStringLike(..), fromChar, toChar, parseT, parse
-  , inspect, token, tokens, eof, empty, (<|>), choice, many, some
-  , parens, setAllowBadCP, flush ) where
+  , inspect, token, tokens, eof, neg, lookAhead, empty, (<|>), choice, many
+  , some, parens, setAllowBadCP, flush, (<&>)
+  , range, sepBy, sepBy1, sepEndBy, sepEndBy1 ) where
 
 import           MMZKBSParser.Convert
 import           MMZKBSParser.Parser
---
+++ test/Hex.hs
@@ -0,0 +1,39 @@
+import           MMZKBSParser
+import qualified MMZKBSParser.Lexer as L
+import           Test.HUnit
+
+main = print (parseHex "1123Bd13")
+
+-- main :: IO ()
+-- main = runTestTTAndExit
+--      $ TestList [ testValids "Basic pairs" basicPairs
+--                 , testValids "Advanced pairs" advancedPairs
+--                 , testInvalids "Malformed pairs" malformedPairs ]
+--   where
+--     basicPairs     = [simpleParens, simpleBraces, simpleBracks, simpleChevrs]
+--     advancedPairs  = [consecutivePairs, nestedPairs, combo]
+--     malformedPairs = [mismatchPairs, badOrder]
+
+-- testValids :: String -> [String] -> Test
+-- testValids = (. TestList . map testSingleValid) . TestLabel
+
+-- testInvalids :: String -> [String] -> Test
+-- testInvalids = (. TestList . map testSingleInvalid) . TestLabel
+
+-- testSingleValid :: String -> Test
+-- testSingleValid kuohu = TestCase
+--                       $ assertEqual ("Test parsing \"" ++ kuohu ++ "\":")
+--                                     (Just kuohu)
+--                                     (fmap formatKuohu (parseKuohu kuohu))
+
+-- testSingleInvalid :: String -> Test
+-- testSingleInvalid kuohu = TestCase
+--                         $ assertEqual ("Test parsing \"" ++ kuohu ++ "\":")
+--                                       Nothing
+--                                       (fmap formatKuohu (parseKuohu kuohu))
+
+parseHex :: ByteStringLike s => s -> Maybe String
+parseHex = parse (hexParser <* eof)
+
+hexParser :: Parser String
+hexParser = many (L.hexDigit <&> [neg L.lower])
