2674349c15de266e7b24cd06684e7b13776e5c5f
mmzk1526 <yc4120@ic.ac.uk>
2022-07-11 01:09:24 +0800

    [yc4120] (feat) ASCII and Latin-1 parsers for 8-bit Char

--- src/MMZKBSParser/Word8.hs
+++ src/MMZKBSParser/Word8.hs
@@ -40,7 +40,7 @@ space = satisfyL1 (\ch -> ch == 32 || (ch >= 9 && ch <= 13))
 -- | Parse a single digit: 0, 1, 2, 3, 4, 5, 6, 7, 8, or 9. The result is
 -- retained as a "Word8".
 digit :: Monad m => ParserT m Word8
-digit = satisfy (\ch -> ch >= 48 && ch <= 57)
+digit = satisfyL1 (\ch -> ch >= 48 && ch <= 57)
 {-# INLINE digit #-}
 
 -- | Parse a single hexadecimal digit: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9; a, b, c, d,
@@ -85,35 +85,35 @@ ascii :: Monad m => ParserT m Word8
 ascii = satisfy $ const True
 {-# INLINE ascii #-}
 
--- | Parse a single letter: a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q,
--- r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q,
--- R, S, T, U, V, W, X, Y, Z. The result is retained as a "Word8".
+-- | Parse a single ASCII letter: a, b, c, d, e, f, g, h, i, j, k, l, m, n, o,
+-- p, q, r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L, M, N, O,
+-- P, Q, R, S, T, U, V, W, X, Y, Z. The result is retained as a "Word8".
 alpha :: Monad m => ParserT m Word8
-alpha = satisfy (\ch -> (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+alpha = satisfyL1 (\ch -> (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
 {-# INLINE alpha #-}
 
--- | Parse a single Latin letter or digit: a, b, c, d, e, f, g, h, i, j, k, l,
+-- | Parse a single ASCII letter or digit: a, b, c, d, e, f, g, h, i, j, k, l,
 -- m, n, o, p, q, r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L,
 -- M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9. The
 -- result is retained as a "Word8".
 alphaNum :: Monad m => ParserT m Word8
-alphaNum = satisfy ( \ch -> (ch >= 48 && ch <= 57)
-                         || (ch >= 65 && ch <= 90)
-                         || (ch >= 97 && ch <= 122) )
+alphaNum = satisfyL1 ( \ch -> (ch >= 48 && ch <= 57)
+                           || (ch >= 65 && ch <= 90)
+                           || (ch >= 97 && ch <= 122) )
 {-# INLINE alphaNum #-}
 
--- | Parse a single lowercase Latin letter: a, b, c, d, e, f, g, h, i, j, k, l,
+-- | Parse a single lowercase ASCII letter: a, b, c, d, e, f, g, h, i, j, k, l,
 -- m, n, o, p, q, r, s, t, u, v, w, x, y, or z; The result is retained as a
 -- "Word8".
 lower :: Monad m => ParserT m Word8
-lower = satisfy (\ch -> ch >= 97 && ch <= 122)
+lower = satisfyL1 (\ch -> ch >= 97 && ch <= 122)
 {-# INLINE lower #-}
 
--- | Parse a single uppercase Latin letter: A, B, C, D, E, F, G, H, I, J, K, L,
+-- | Parse a single uppercase ASCII letter: A, B, C, D, E, F, G, H, I, J, K, L,
 -- M, N, O, P, Q, R, S, T, U, V, W, X, Y, or Z. The result is retained as a
 -- "Word8".
 upper :: Monad m => ParserT m Word8
-upper = satisfy (\ch -> ch >= 65 && ch <= 90)
+upper = satisfyL1 (\ch -> ch >= 65 && ch <= 90)
 {-# INLINE upper #-}
 
 
@@ -138,16 +138,16 @@ noneOfL1 chs = satisfy (`notElem` chs)
 {-# INLINE noneOfL1 #-}
 
 -- | Parse any token ("Word8"). The result is retained as a "Word8".
-anyToken :: Monad m => ParserT m Word8
-anyToken = token Just
-{-# INLINE anyToken #-}
+latin1 :: Monad m => ParserT m Word8
+latin1 = token Just
+{-# INLINE latin1 #-}
 
--- | Parse a single letter: a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q,
--- r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q,
--- R, S, T, U, V, W, X, Y, Z; À, Á, Â, Ã, Ä, Å, Æ, Ç, È, É, Ê, Ë, Ì, Í, Î, Ï, Ð,
--- Ñ, Ò, Ó, Ô, Õ, Ö, Ø, Ù, Ú, Û, Ü, Ý; à, á, â, ã, ä, å, æ, ç, è, é, ê, ë, ì, í,
--- î, ï, ð, ñ, ò, ó, ô, õ, ö, ø, ù, ú, û, ü, ý; ª, µ, º, Þ, ß, þ, ÿ. The result
--- is retained as a "Word8".
+-- | Parse a single Latin-1 letter: a, b, c, d, e, f, g, h, i, j, k, l, m, n, o,
+-- p, q, r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L, M, N, O,
+-- P, Q, R, S, T, U, V, W, X, Y, Z; À, Á, Â, Ã, Ä, Å, Æ, Ç, È, É, Ê, Ë, Ì, Í, Î,
+-- Ï, Ð, Ñ, Ò, Ó, Ô, Õ, Ö, Ø, Ù, Ú, Û, Ü, Ý; à, á, â, ã, ä, å, æ, ç, è, é, ê, ë,
+-- ì, í, î, ï, ð, ñ, ò, ó, ô, õ, ö, ø, ù, ú, û, ü, ý; ª, µ, º, Þ, ß, þ, ÿ. The
+-- result is retained as a "Word8".
 alphaL1 :: Monad m => ParserT m Word8
 alphaL1 = satisfyL1 ( \ch -> (ch >= 65 && ch <= 90)
                  || (ch >= 97 && ch <= 122)
@@ -155,7 +155,7 @@ alphaL1 = satisfyL1 ( \ch -> (ch >= 65 && ch <= 90)
                  || (ch >= 192 && ch /= 215 && ch /= 247) )
 {-# INLINE alphaL1 #-}
 
--- | Parse a single Latin letter or digit: a, b, c, d, e, f, g, h, i, j, k, l,
+-- | Parse a single Latin-1 letter or digit: a, b, c, d, e, f, g, h, i, j, k, l,
 -- m, n, o, p, q, r, s, t, u, v, w, x, y, z; A, B, C, D, E, F, G, H, I, J, K, L,
 -- M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z; À, Á, Â, Ã, Ä, Å, Æ, Ç, È, É, Ê, Ë,
 -- Ì, Í, Î, Ï, Ð, Ñ, Ò, Ó, Ô, Õ, Ö, Ø, Ù, Ú, Û, Ü, Ý; à, á, â, ã, ä, å, æ, ç, è,
@@ -169,9 +169,9 @@ alphaNumL1 = satisfyL1 ( \ch -> (ch >= 48 && ch <= 57)
                              || (ch >= 192 && ch /= 215 && ch /= 247) )
 {-# INLINE alphaNumL1 #-}
 
--- | Parse a single lowercase Latin letter: a, b, c, d, e, f, g, h, i, j, k, l,
--- m, n, o, p, q, r, s, t, u, v, w, x, y, z; ; à, á, â, ã, ä, å, æ, ç, è, é, ê,
--- ë, ì, í, î, ï, ð, ñ, ò, ó, ô, õ, ö, ø, ù, ú, û, ü, ý; µ, ß, ÿ. The result is 
+-- | Parse a single lowercase Latin-1 letter: a, b, c, d, e, f, g, h, i, j, k,
+-- l, m, n, o, p, q, r, s, t, u, v, w, x, y, z; à, á, â, ã, ä, å, æ, ç, è, é, ê,
+-- ë, ì, í, î, ï, ð, ñ, ò, ó, ô, õ, ö, ø, ù, ú, û, ü, ý; µ, ß, ÿ. The result is
 -- retained as a "Word8".
 lowerL1 :: Monad m => ParserT m Word8
 lowerL1 = satisfyL1 ( \ch -> (ch >= 97 && ch <= 122)
@@ -179,10 +179,10 @@ lowerL1 = satisfyL1 ( \ch -> (ch >= 97 && ch <= 122)
                           || (ch >= 223 && ch /= 247) )
 {-# INLINE lowerL1 #-}
 
--- | Parse a single uppercase Latin letter: A, B, C, D, E, F, G, H, I, J, K, L,
--- M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z; À, Á, Â, Ã, Ä, Å, Æ, Ç, È, É, Ê, Ë,
--- Ì, Í, Î, Ï, Ð, Ñ, Ò, Ó, Ô, Õ, Ö, Ø, Ù, Ú, Û, Ü, Ý; Þ. The result is retained
--- as a "Word8".
+-- | Parse a single uppercase Latin-1 letter: A, B, C, D, E, F, G, H, I, J, K,
+-- L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z; À, Á, Â, Ã, Ä, Å, Æ, Ç, È, É, Ê,
+-- Ë, Ì, Í, Î, Ï, Ð, Ñ, Ò, Ó, Ô, Õ, Ö, Ø, Ù, Ú, Û, Ü, Ý; Þ. The result is
+-- retained as a "Word8".
 upperL1 :: Monad m => ParserT m Word8
 upperL1 = satisfyL1 ( \ch -> (ch >= 65 && ch <= 90) 
                           || (ch >= 192 && ch <= 222 && ch /= 215) )
