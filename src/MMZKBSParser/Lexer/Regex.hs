8f153343aa7eaf534e04289d531f3139578bda22
mmzk1526 <yc4120@ic.ac.uk>
2022-07-13 09:49:20 +0800

    [yc4120] (feat) Parse ByteString according to the Regex construct

--- src/MMZKBSParser/Lexer/Regex.hs
+++ src/MMZKBSParser/Lexer/Regex.hs
@@ -1,13 +1,31 @@
 module MMZKBSParser.Lexer.Regex where
 
 import           Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import           Data.Maybe
+import           MMZKBSParser.Convert
+import           MMZKBSParser.Parser
+import qualified MMZKBSParser.Lexer as L
 
-data Regex = Literal ByteString -- ^ abc
-           | OneOf   ByteString -- ^ [abc]
-           | NoneOf  ByteString -- ^ [^abc]
-           | Many    Regex      -- ^ a*
-           | Some    Regex      -- ^ a+
-           | Couldbe Regex      -- ^ a?
-           | Choice  [Regex]    -- ^ [ab|cd|ef]
-           | AnyChar            -- ^ .
-           | EOF                -- ^ $
+data Regex = Literal  ByteString -- ^ abc
+           | OneOf    [Char]     -- ^ [abc]
+           | NoneOf   [Char]     -- ^ [^abc]
+           | Many     Regex      -- ^ a*
+           | Some     Regex      -- ^ a+
+           | Optional Regex      -- ^ a?
+           | Choice   [Regex]    -- ^ [ab|cd|ef]
+           | Sequence [Regex]    -- ^ [abc][def]
+           | AnyChar             -- ^ .
+
+-- | Parse a "ByteString" according to the given "Regex".
+regexParser :: Monad m => Regex -> ParserT m ByteString
+regexParser (Literal str)      = L.byteString str
+regexParser (OneOf chs)        = toByteString . (: []) <$> L.oneOf chs
+regexParser (NoneOf chs)       = toByteString . (: []) <$> L.noneOf chs
+regexParser (Many regex)       = BS.concat <$> many (regexParser regex)
+regexParser (Some regex)       = BS.concat <$> some (regexParser regex)
+regexParser (Optional regex)   = fromMaybe BS.empty
+                             <$> optional (regexParser regex)
+regexParser (Choice regexes)   = choice $ regexParser <$> regexes
+regexParser (Sequence regexes) = BS.concat <$> mapM regexParser regexes
+regexParser AnyChar            = toByteString . (: []) <$> L.anyChar
