3a59d7ac44dd557170ce30f3b4b319256ac5f21e
mmzk1526 <yc4120@ic.ac.uk>
2022-07-22 01:03:11 +0800

    [mmzk] (feat) Better debugging message

--- src/MMZK/BSParser/Debug.hs
+++ src/MMZK/BSParser/Debug.hs
@@ -1,11 +1,22 @@
 {-# LANGUAGE CPP  #-}
 
-module MMZK.BSParser.Debug where
+module MMZK.BSParser.Debug (__ASSERT__, __UNREACHABLE__) where
+
+#define __POS__ (++ (__FILE__ ++ ": line " ++ show (__LINE__ :: Integer)))
 
 __UNREACHABLE__ :: a -> a
 #ifdef ASSERTS
-__UNREACHABLE__ = error "Assertion Error: Unreachable!"
+__UNREACHABLE__ = error . __POS__ $ "Assertion Error: Unreachable!\n"
 #else
 __UNREACHABLE__ = id
 #endif
 {-# INLINE __UNREACHABLE__ #-}
+
+__ASSERT__ :: Bool -> a -> a
+#ifdef ASSERTS
+__ASSERT__ True  = id
+__ASSERT__ False = error . __POS__ $ "Assertion Error: Assertion failed!\n"
+#else
+__ASSERT__ = const id
+#endif
+{-# INLINE __ASSERT__ #-}
