07ca50fb8d8457cfc6a846647dce5675b338cc1b
mmzk1526 <yc4120@ic.ac.uk>
2022-07-24 22:40:45 +0800

    [mmzk] (refactor) Remove explict pruning for combinators

--- src/MMZK/BSParser/CPS.hs
+++ src/MMZK/BSParser/CPS.hs
@@ -8,7 +8,6 @@ import           Control.Monad
 import           Control.Applicative (Alternative, liftA2, (<|>))
 import qualified MMZK.BSParser.Lexer as L
 import           MMZK.BSParser.Parser (BSParserT)
-import qualified MMZK.BSParser.Parser as P
 
 
 --------------------------------------------------------------------------------
@@ -26,7 +25,7 @@ cons = liftA2 ((<|>) . pure)
 -- second argument.
 many :: Alternative t => Foldable t => Monad m
      => BSParserT e m a -> BSParserT e m (t a) -> BSParserT e m (t a)
-many p q = msum [liftA2 ((<|>) . pure) p (P.prune >> many p q), q]
+many p q = msum [liftA2 ((<|>) . pure) p (many p q), q]
 {-# NOINLINE [2] many #-}
 {-# RULES "many/[]" [~2] many = manyL #-}
 
@@ -35,7 +34,7 @@ many p q = msum [liftA2 ((<|>) . pure) p (P.prune >> many p q), q]
 some :: Alternative t => Foldable t => Monad m
      => BSParserT e m a -> BSParserT e m (t a) -> BSParserT e m (t a)
 some p q = liftA2 ((<|>) . pure) p 
-         $ msum [liftA2 ((<|>) . pure) p (P.prune >> many p q), q]
+         $ msum [liftA2 ((<|>) . pure) p (many p q), q]
 {-# NOINLINE [2] some #-}
 {-# RULES "some/[]" [~2] some = someL #-}
 
@@ -50,7 +49,7 @@ manyS pCPS q = msum [pCPS $ msum [manyS pCPS q, q], q]
 someS :: Alternative t => Foldable t => Monad m
       => (BSParserT e m (t a) -> BSParserT e m (t a))
       -> (BSParserT e m (t a) -> BSParserT e m (t a))
-someS pCPS q = pCPS $ msum [manyS pCPS q, q]
+someS pCPS q = pCPS $ msum [someS pCPS q, q]
 {-# NOINLINE [2] someS #-}
 
 consL :: Monad m  => BSParserT e m a -> BSParserT e m [a] -> BSParserT e m [a]
@@ -58,10 +57,10 @@ consL = liftA2 (:)
 {-# INLINE [2] consL #-}
 
 manyL :: Monad m => BSParserT e m a -> BSParserT e m [a] -> BSParserT e m [a]
-manyL p q = msum [liftA2 (:) p (P.prune >> many p q), q]
+manyL p q = msum [liftA2 (:) p (many p q), q]
 
 someL :: Monad m => BSParserT e m a -> BSParserT e m [a] -> BSParserT e m [a]
-someL p q = liftA2 (:) p $ msum [liftA2 (:) p (P.prune >> many p q), q]
+someL p q = liftA2 (:) p $ msum [liftA2 (:) p (many p q), q]
 
 
 --------------------------------------------------------------------------------
